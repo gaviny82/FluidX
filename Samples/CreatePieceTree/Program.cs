6aba536c3d58ccedcbe206371732c86ede915e1f
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2024-10-21 22:37:51 +0100

    Add sample

---
+++ Samples/CreatePieceTree/Program.cs
@@ -0,0 +1,13 @@
+﻿using EditorTextBuffers;
+using EditorTextBuffers.PieceTree;
+
+var pieceTreeBuilder = new PieceTreeTextBufferBuilder();
+pieceTreeBuilder.AcceptChunk("abc\n");
+pieceTreeBuilder.AcceptChunk("def");
+var pieceTreeFactory = pieceTreeBuilder.Finish(true);
+var pieceTree = pieceTreeFactory.Create(DefaultEndOfLine.LF);
+
+Console.WriteLine(pieceTree.LineCount); // 2
+Console.WriteLine(pieceTree.GetLineContent(1)); // abc
+Console.WriteLine(pieceTree.GetLineContent(2)); // def
+Console.WriteLine(pieceTree.GetValueInRange(new EditorTextBuffers.Range(1, 1, 2, 2), EndOfLinePreference.LF)); // abc\nd
