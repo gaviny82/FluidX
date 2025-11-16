0656a37e04f8e9c3162920279969e76a22f9eafb
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2025-11-16 13:31:06 +0800

    Port token store types (#4)
    * Add LineTokens and TokenArray
    
    * Add ContiguousTokensStore
    
    * Add ContiguousMultilineTokens
    
    * Add SparseMultilineTokens
    
    * Add SparseTokensStore

---
+++ src/Tokenization/TokenStores/LineToken.cs
@@ -0,0 +1,194 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FluidX.Tokenization.TokenStores;
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public readonly struct LineTokenMetadata(uint metadata)
+{
+    private readonly uint _metadata = metadata;
+
+    public uint RawValue => _metadata;
+
+    public LanguageId LanguageId
+        => (LanguageId)((_metadata & MetadataConsts.LANGUAGEID_MASK) >> MetadataConsts.LANGUAGEID_OFFSET);
+
+    public StandardTokenType TokenType
+        => (StandardTokenType)((_metadata & MetadataConsts.TOKEN_TYPE_MASK) >> MetadataConsts.TOKEN_TYPE_OFFSET);
+
+    public bool ContainsBalancedBrackets
+        => ((_metadata & MetadataConsts.BALANCED_BRACKETS_MASK) >> MetadataConsts.BALANCED_BRACKETS_OFFSET) != 0;
+
+    public FontStyle FontStyle
+        => (FontStyle)((_metadata & MetadataConsts.FONT_STYLE_MASK) >> MetadataConsts.FONT_STYLE_OFFSET);
+
+    public ColorId Foreground
+        => (ColorId)((_metadata & MetadataConsts.FOREGROUND_MASK) >> MetadataConsts.FOREGROUND_OFFSET);
+
+    public ColorId Background
+        => (ColorId)((_metadata & MetadataConsts.BACKGROUND_MASK) >> MetadataConsts.BACKGROUND_OFFSET);
+
+    public string GetClassName()
+    {
+        string className = $"mtk{Foreground}";
+        FontStyle fontStyle = FontStyle;
+        if (fontStyle.HasFlag(FontStyle.Italic))
+            className += " mtki";
+        if (fontStyle.HasFlag(FontStyle.Bold))
+            className += " mtkb";
+        if (fontStyle.HasFlag(FontStyle.Underline))
+            className += " mtku";
+        if (fontStyle.HasFlag(FontStyle.Strikethrough))
+            className += " mtks";
+        return className;
+    }
+
+    public string GetInlineStyle(string[] colorMap)
+    {
+        var foreground = Foreground;
+        var fontStyle = FontStyle;
+
+        string result = $"color: {colorMap[(int)foreground]};";
+        if (fontStyle.HasFlag(FontStyle.Italic))
+        {
+            result += "font-style: italic;";
+        }
+        if (fontStyle.HasFlag(FontStyle.Bold))
+        {
+            result += "font-weight: bold;'";
+        }
+        string textDecoration = "";
+        if (fontStyle.HasFlag(FontStyle.Underline))
+        {
+            textDecoration += " underline";
+        }
+        if (fontStyle.HasFlag(FontStyle.Strikethrough))
+        {
+            textDecoration += " line-through";
+        }
+        if (!string.IsNullOrEmpty(textDecoration))
+        {
+            result += $"text-decoration:{textDecoration};";
+        }
+        return result;
+    }
+
+    public TokenPresentation GetPresentation()
+    {
+        var foreground = Foreground;
+        var fontStyle = FontStyle;
+        return new TokenPresentation(
+            foreground,
+            fontStyle.HasFlag(FontStyle.Italic),
+            fontStyle.HasFlag(FontStyle.Bold),
+            fontStyle.HasFlag(FontStyle.Underline),
+            fontStyle.HasFlag(FontStyle.Strikethrough)
+        );
+    }
+}
+
+public record struct TokenPresentation(ColorId Foreground, bool Italic, bool Bold, bool Underline, bool Strikethrough);
+
+[StructLayout(LayoutKind.Sequential, Pack = 1)]
+public readonly record struct LineToken(int EndOffset, LineTokenMetadata Metadata);
+
+/**
+ * Helpers to manage the "collapsed" metadata of an entire StackElement stack.
+ * The following assumptions have been made:
+ *  - languageId < 256 => needs 8 bits
+ *  - unique color count < 512 => needs 9 bits
+ *
+ * The binary format is:
+ * - -------------------------------------------
+ *     3322 2222 2222 1111 1111 1100 0000 0000
+ *     1098 7654 3210 9876 5432 1098 7654 3210
+ * - -------------------------------------------
+ *     xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+ *     bbbb bbbb ffff ffff fFFF FBTT LLLL LLLL
+ * - -------------------------------------------
+ *  - L = LanguageId (8 bits)
+ *  - T = StandardTokenType (2 bits)
+ *  - B = Balanced bracket (1 bit)
+ *  - F = FontStyle (4 bits)
+ *  - f = foreground color (9 bits)
+ *  - b = background color (8 bits)
+ *
+ */
+public static class MetadataConsts
+{
+    public const uint LANGUAGEID_MASK /*            */ = 0b00000000_00000000_00000000_11111111;
+    public const uint TOKEN_TYPE_MASK /*            */ = 0b00000000_00000000_00000011_00000000;
+    public const uint BALANCED_BRACKETS_MASK /*     */ = 0b00000000_00000000_00000100_00000000;
+    public const uint FONT_STYLE_MASK /*            */ = 0b00000000_00000000_01111000_00000000;
+    public const uint FOREGROUND_MASK /*            */ = 0b00000000_11111111_10000000_00000000;
+    public const uint BACKGROUND_MASK /*            */ = 0b11111111_00000000_00000000_00000000;
+
+    public const uint ITALIC_MASK /*                */ = 0b00000000_00000000_00001000_00000000;
+    public const uint BOLD_MASK /*                  */ = 0b00000000_00000000_00010000_00000000;
+    public const uint UNDERLINE_MASK /*             */ = 0b00000000_00000000_00100000_00000000;
+    public const uint STRIKETHROUGH_MASK /*         */ = 0b00000000_00000000_01000000_00000000;
+
+    // Semantic tokens cannot set the language id, so we can
+    // use the first 8 bits for control purposes
+    public const uint SEMANTIC_USE_ITALIC /*        */ = 0b00000000_00000000_00000000_00000001;
+    public const uint SEMANTIC_USE_BOLD /*          */ = 0b00000000_00000000_00000000_00000010;
+    public const uint SEMANTIC_USE_UNDERLINE  /*    */ = 0b00000000_00000000_00000000_00000100;
+    public const uint SEMANTIC_USE_STRIKETHROUGH /* */ = 0b00000000_00000000_00000000_00001000;
+    public const uint SEMANTIC_USE_FOREGROUND /*    */ = 0b00000000_00000000_00000000_00010000;
+    public const uint SEMANTIC_USE_BACKGROUND /*    */ = 0b00000000_00000000_00000000_00100000;
+
+    public const int LANGUAGEID_OFFSET = 0;
+    public const int TOKEN_TYPE_OFFSET = 8;
+    public const int BALANCED_BRACKETS_OFFSET = 10;
+    public const int FONT_STYLE_OFFSET = 11;
+    public const int FOREGROUND_OFFSET = 15;
+    public const int BACKGROUND_OFFSET = 24;
+}
+
+/**
+ * Open ended enum at runtime
+ */
+public enum LanguageId
+{
+    Null = 0,
+    PlainText = 1
+}
+
+/**
+ * A font style. Values are 2^x such that a bit mask can be used.
+ */
+public enum FontStyle
+{
+    NotSet = -1,
+    None = 0,
+    Italic = 1,
+    Bold = 2,
+    Underline = 4,
+    Strikethrough = 8,
+}
+
+/**
+ * Open ended enum at runtime
+ */
+public enum ColorId
+{
+    None = 0,
+    DefaultForeground = 1,
+    DefaultBackground = 2
+}
+
+/**
+ * A standard token type.
+ */
+public enum StandardTokenType
+{
+    Other = 0,
+    Comment = 1,
+    String = 2,
+    RegEx = 3
+}
+
+public interface ILanguageIdCodec
+{
+    LanguageId EncodeLanguageId(string languageId);
+    string DecodeLanguageId(LanguageId languageId);
+}
---
+++ src/Tokenization/TokenStores/LineTokens.cs
@@ -0,0 +1,310 @@
+﻿using System.Collections;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FluidX.Tokenization.TokenStores;
+
+public class LineTokens : IEnumerable<LineToken>
+{
+    private readonly LineToken[] _tokens;
+    private readonly string _text;
+    public ILanguageIdCodec LanguageIdCodec { get; init; }
+
+    public string LineContent => _text;
+    public int TextLength => _text.Length;
+    public int Count => _tokens.Length;
+
+    public LineTokens(LineToken[] tokens, string text, ILanguageIdCodec decoder)
+    {
+        _tokens = tokens;
+        _text = text;
+        LanguageIdCodec = decoder;
+    }
+
+    public static LineTokenMetadata DefaultTokenMetadata = new(
+        ((uint)FontStyle.None << MetadataConsts.FONT_STYLE_OFFSET)
+        | ((uint)ColorId.DefaultForeground << MetadataConsts.FOREGROUND_OFFSET)
+        | ((uint)ColorId.DefaultBackground << MetadataConsts.BACKGROUND_OFFSET)
+    );
+
+    public static LineTokens CreateEmpty(string lineContent, ILanguageIdCodec decoder)
+    {
+        LineToken[] tokens = [new LineToken(lineContent.Length, DefaultTokenMetadata)];
+        return new(tokens, lineContent, decoder);
+    }
+
+    public static LineTokens CreateFromTextAndMetadata((string text, LineTokenMetadata metadata)[] data, ILanguageIdCodec decoder)
+    {
+        int offset = 0;
+        string fullText = "";
+        List<LineToken> tokens = [];
+        foreach(var (text, metadata) in data)
+        {
+            tokens.Add(new(offset + text.Length, metadata));
+            offset += text.Length;
+            fullText += text;
+        }
+        return new(tokens.ToArray(), fullText, decoder);
+    }
+
+    #region Equality
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is not LineTokens other)
+            return false;
+        return other == this;
+    }
+
+    public static bool operator ==(LineTokens left, LineTokens right)
+    {
+        if (left._text !=  right._text)
+            return false;
+        return left._tokens.SequenceEqual(right._tokens);
+    }
+
+    public static bool operator !=(LineTokens left, LineTokens right)
+    {
+        return !(left == right);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_text, _tokens);
+    }
+
+    public bool SlicedEquals(LineTokens other, int sliceFromTokenIndex, int sliceTokenCount)
+    {
+        if (_text != other._text)
+            return false;
+        var thisSlice = _tokens.AsSpan().Slice(sliceFromTokenIndex, sliceTokenCount);
+        var otherSlice = other._tokens.AsSpan().Slice(sliceFromTokenIndex, sliceTokenCount);
+        return thisSlice.SequenceEqual(otherSlice);
+    }
+
+    #endregion
+
+    public int GetStartOffset(int tokenIndex)
+    {
+        if (tokenIndex <= 0)
+            return 0;
+        return _tokens[tokenIndex - 1].EndOffset;
+    }
+
+    public int GetEndOffset(int tokenIndex)
+    {
+        return _tokens[tokenIndex].EndOffset;
+    }
+
+    public LineTokenMetadata GetMetadata(int tokenIndex)
+    {
+        return _tokens[tokenIndex].Metadata;
+    }
+
+    public string GetLanguageId(int tokenIndex)
+    {
+        LanguageId langId = _tokens[tokenIndex].Metadata.LanguageId;
+        return LanguageIdCodec.DecodeLanguageId(langId);
+    }
+
+    /// <summary>
+    /// Find the token containing offset `offset`.
+    /// </summary>
+    /// <param name="offset">The search offset</param>
+    /// <returns>The index of the token containing the offset.</returns>
+    public int FindTokenIndexAtOffset(int offset)
+        => FindIndexInTokensArray(_tokens, offset);
+
+    public static int FindIndexInTokensArray(LineToken[] tokens, int offset)
+    {
+        // Binary search
+        int low = 0;
+        int high = tokens.Length - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            int midEndOffset = tokens[mid].EndOffset;
+            if (offset < midEndOffset)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+
+    public ReadOnlySpan<char> GetTokenText(int tokenIndex)
+    {
+        int startOffset = GetStartOffset(tokenIndex);
+        int endOffset = GetEndOffset(tokenIndex);
+        return _text.AsSpan(startOffset, endOffset - startOffset);
+    }
+
+    public SliceLineTokens Slice(int startOffset, int endOffset, int deltaOffset)
+        => new(this, startOffset, endOffset, deltaOffset);
+
+    public TokenArray GetTokensInRange(Range range)
+        => new(Slice(0, Count, 0));
+
+    /// <summary>
+    /// </summary>
+    /// <param name="insertTokens">Must be sorted by offset.</param>
+    /// <returns></returns>
+    public LineTokens WithInserted((int offset, string text, LineTokenMetadata metadata)[] insertTokens)
+    {
+        if (insertTokens.Length == 0)
+            return this;
+
+        int nextOriginalTokenIdx = 0;
+        int nextInsertTokenIdx = 0;
+        string text = "";
+        List<LineToken> newTokens = [];
+
+        int originalEndOffset = 0;
+        while (true)
+        {
+            int nextOriginalTokenEndOffset = nextOriginalTokenIdx < _tokens.Length ? _tokens[nextOriginalTokenIdx].EndOffset : -1;
+            (int offset, string text, LineTokenMetadata metadata)? nextInsertToken = nextInsertTokenIdx < insertTokens.Length ? insertTokens[nextInsertTokenIdx] : null;
+
+            if (nextOriginalTokenEndOffset != -1 && (nextInsertToken == null || nextOriginalTokenEndOffset <= nextInsertToken?.offset))
+            {
+                // original token ends before next insert token
+                text += _text.Substring(originalEndOffset, nextOriginalTokenEndOffset - originalEndOffset);
+                var metadata = _tokens[nextOriginalTokenIdx].Metadata;
+                newTokens.Add(new(text.Length, metadata));
+                nextOriginalTokenIdx++;
+                originalEndOffset = nextOriginalTokenEndOffset;
+            }
+            else if (nextInsertToken is not null)
+            {
+                if (nextInsertToken?.offset > originalEndOffset)
+                {
+                    // insert token is in the middle of the next token.
+                    text += _text.Substring(originalEndOffset, (int)nextInsertToken?.offset! - originalEndOffset);
+                    var metadata = _tokens[nextOriginalTokenIdx].Metadata;
+                    newTokens.Add(new (text.Length, metadata));
+                    originalEndOffset = (int)nextInsertToken?.offset!;
+                }
+
+                text += nextInsertToken?.text;
+                newTokens.Add(new (text.Length, (LineTokenMetadata)nextInsertToken?.metadata!));
+                nextInsertTokenIdx++;
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return new LineTokens(newTokens.ToArray(), text, LanguageIdCodec);
+    }
+
+    #region IEnumerable Members
+
+    public IEnumerator<LineToken> GetEnumerator()
+    {
+        return ((IEnumerable<LineToken>)_tokens).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return _tokens.GetEnumerator();
+    }
+
+    #endregion
+}
+
+public readonly struct SliceLineTokens
+{
+    private readonly LineTokens _source;
+    private readonly int _startOffset;
+    private readonly int _endOffset;
+    private readonly int _deltaOffset;
+
+    private readonly int _firstTokenIndex;
+    private readonly int _tokensCount;
+
+    public ILanguageIdCodec LanguageIdCodec => _source.LanguageIdCodec;
+
+    public SliceLineTokens(LineTokens source, int startOffset, int endOffset, int deltaOffset)
+    {
+        _source = source;
+        _startOffset = startOffset;
+        _endOffset = endOffset;
+        _deltaOffset = deltaOffset;
+
+        _firstTokenIndex = source.FindTokenIndexAtOffset(startOffset);
+        _tokensCount = 0;
+        for (int i = 0; i < source.Count; i++)
+        {
+            int tokenStartOffset = source.GetStartOffset(i);
+            if (tokenStartOffset >= endOffset)
+                break;
+            _tokensCount++;
+        }
+    }
+
+    public LineTokenMetadata GetMetadata(int tokenIndex)
+        => _source.GetMetadata(_firstTokenIndex + tokenIndex);
+
+    public ReadOnlySpan<char> LineContent
+        => _source.LineContent.AsSpan()[_startOffset.._endOffset];
+
+    public int Count => _tokensCount;
+
+    public override bool Equals([NotNullWhen(true)] object? obj)
+    {
+        if (!(obj is SliceLineTokens other))
+            return false;
+        return other.Equals(this);
+    }
+
+    public bool Equals(SliceLineTokens other)
+    {
+        return _startOffset == other._startOffset
+            && _endOffset == other._endOffset
+            && _deltaOffset == other._deltaOffset
+            && _source.SlicedEquals(other._source, _firstTokenIndex, _tokensCount);
+    }
+
+    public int GetStartOffset(int tokenIndex)
+    {
+        int tokenStartOffset = _source.GetStartOffset(_firstTokenIndex + tokenIndex);
+        return Math.Max(tokenStartOffset, _startOffset) - _startOffset + _deltaOffset;
+    }
+
+    public int GetEndOffset(int tokenIndex)
+    {
+        int tokenEndOffset = _source.GetEndOffset(_firstTokenIndex + tokenIndex);
+        return Math.Min(tokenEndOffset, _endOffset) - _startOffset + _deltaOffset;
+    }
+
+    public int FindTokenIndexAtOffset(int offset)
+        => _source.FindTokenIndexAtOffset(offset + _startOffset - _deltaOffset) - _firstTokenIndex;
+
+    public ReadOnlySpan<char> GetTokenText(int tokenIndex)
+    {
+        int adjustedTokenIndex = _firstTokenIndex + tokenIndex;
+        int tokenStartOffset = _source.GetStartOffset(adjustedTokenIndex);
+        int tokenEndOffset = _source.GetEndOffset(adjustedTokenIndex);
+        var text = _source.GetTokenText(adjustedTokenIndex);
+        if (tokenStartOffset < _startOffset)
+            text = text.Slice(_startOffset - tokenStartOffset);
+        if(tokenEndOffset > _endOffset)
+            text = text.Slice(0, text.Length - (tokenEndOffset - _endOffset));
+        return text;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(_source, _startOffset, _endOffset, _deltaOffset);
+    }
+
+    public static bool operator ==(SliceLineTokens left, SliceLineTokens right)
+    {
+        return left.Equals(right);
+    }
+
+    public static bool operator !=(SliceLineTokens left, SliceLineTokens right)
+    {
+        return !(left == right);
+    }
+}
