499fb110c80daec5cc3cda2f9a136f84926a7b4b
gaviny82 <38808970+gaviny82@users.noreply.github.com>
2025-11-23 15:37:10 +0800

    Add extensions for ITextPosition

--- src/ITextPosition.cs
+++ src/ITextPosition.cs
@@ -15,3 +15,51 @@ public interface ITextPosition
     /// </summary>
     int Column { get; }
 }
+
+public static class ITextPositionExtensions
+{
+    extension(ITextPosition pos)
+    {
+        /// <summary>
+        /// Test if position <paramref name="a"/> equals position <paramref name="otherPos"/>.
+        /// </summary>
+        /// <returns>If the two positions are equal, the result will be <see langword="false"/>.</returns>
+        public bool IsBefore(ITextPosition otherPos)
+            => pos.Lift().IsBefore(otherPos.Lift());
+
+        /// <summary>
+        /// Test if position <paramref name="a"/> is before or equal to position <paramref name="otherPos"/>.
+        /// </summary>
+        /// <returns>If the two positions are equal, the result will be <see langword="true"/>.</returns>
+        public bool IsBeforeOrEqual(ITextPosition otherPos)
+            => pos.Lift().IsBeforeOrEqual(otherPos.Lift());
+
+        /// <summary>
+        /// Compare two positions for sorting.
+        /// </summary>
+        public int CompareTo(ITextPosition otherPos)
+            => pos.Lift().CompareTo(otherPos.Lift());
+
+        /// <summary>
+        /// Create a <see cref="TextPosition"/> from an <see cref="ITextPosition"/>.
+        /// </summary>
+        public TextPosition Lift()
+            => new(pos.LineNumber, pos.Column);
+    }
+
+    extension(ITextPosition? pos)
+    {
+        /// <summary>
+        /// Test if this position equals another position.
+        /// </summary>
+        /// <param name="otherPos">Other position</param>
+        public bool Equals(ITextPosition? otherPos)
+        {
+            if (pos is null && otherPos is null)
+                return true;
+            if (pos is null || otherPos is null)
+                return false;
+            return pos.Lift().Equals(otherPos.Lift());
+        }
+    }
+}
