a9987b916824156dd076b8c2d51a3967c584e44d
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2026-08-08 17:21:47 +0800

    Add test files downloader

--- benchmarks/FluidX.TextBeffers.Benchmarks/Program.cs
+++ benchmarks/FluidX.TextBeffers.Benchmarks/Program.cs
@@ -1,7 +1,39 @@
-﻿using BenchmarkDotNet.Running;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using BenchmarkDotNet.Running;
+using FluidX.TextBeffers.Benchmarks.Utils;
 
 // Run all benchmarks: dotnet run -c Release --filter "*"
 // Run a specific benchmark: dotnet run -c Release --filter "*ReadLineBenchmark*"
 // List benchmarks: dotnet run -c Release --list flat
 
+EnsureTestFiles(Environment.CurrentDirectory);
 var _ = BenchmarkRunner.Run(typeof(Program).Assembly);
+
+static void EnsureTestFiles(string baseDir)
+{
+    TestFileHelper.BaseDir = baseDir;
+
+    using var httpClient = new HttpClient { Timeout = TimeSpan.FromMinutes(5) };
+
+    foreach (var (_, url, filename) in TestFileHelper.GetFileMap())
+    {
+        var filePath = Path.Combine(baseDir, filename);
+        if (File.Exists(filePath))
+            continue;
+
+        Console.WriteLine($"Downloading {filename} ...");
+        var sw = Stopwatch.StartNew();
+
+        var response = httpClient.GetAsync(url).GetAwaiter().GetResult();
+        response.EnsureSuccessStatusCode();
+
+        using var fs = new FileStream(filePath, FileMode.Create, FileAccess.Write, FileShare.None);
+        response.Content.CopyToAsync(fs).GetAwaiter().GetResult();
+
+        Console.WriteLine($"Downloaded {filename} ({new FileInfo(filePath).Length} bytes) in {sw.Elapsed.TotalSeconds:F1}s");
+    }
+}
---
+++ benchmarks/FluidX.TextBeffers.Benchmarks/Utils/TestFileHelper.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FluidX.TextBeffers.Benchmarks.Utils;
+
+public enum TestFileType
+{
+    CheckerTs,
+    SqliteC,
+    BilingualDictionary
+}
+
+public static class TestFileHelper
+{
+    public static string BaseDir { get; set; } = Environment.CurrentDirectory;
+
+    private static readonly Dictionary<TestFileType, (string url, string filename)> FileMap = new()
+    {
+        [TestFileType.CheckerTs] = (
+            "https://raw.githubusercontent.com/microsoft/TypeScript/main/src/compiler/checker.ts",
+            "checker.ts"),
+        [TestFileType.SqliteC] = (
+            "https://raw.githubusercontent.com/clibs/sqlite/refs/heads/master/sqlite3.c",
+            "sqlite3.c"),
+        [TestFileType.BilingualDictionary] = (
+            "https://raw.githubusercontent.com/titoBouzout/Dictionaries/master/Russian-English%20Bilingual.dic",
+            "Russian-English Bilingual.dic")
+    };
+
+    public static IEnumerable<(TestFileType Type, string Url, string Filename)> GetFileMap() => FileMap.Select(kv => (kv.Key, kv.Value.url, kv.Value.filename));
+
+    public static string GetFilePath(TestFileType fileType)
+    {
+        var (_, filename) = FileMap[fileType];
+        return Path.Combine(BaseDir, filename);
+    }
+
+    public static byte[] LoadFileBytes(TestFileType fileType)
+    {
+        return File.ReadAllBytes(GetFilePath(fileType));
+    }
+
+    public static string LoadFileText(TestFileType fileType)
+    {
+        return File.ReadAllText(GetFilePath(fileType));
+    }
+
+    public static MemoryStream LoadFileStream(TestFileType fileType)
+    {
+        var bytes = LoadFileBytes(fileType);
+        return new MemoryStream(bytes);
+    }
+}
