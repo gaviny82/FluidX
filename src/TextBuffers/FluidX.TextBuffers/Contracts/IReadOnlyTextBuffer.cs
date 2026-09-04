9aadf044428ba86fe238a4d00a93d31719f45143
Jincheng You <38808970+gaviny82@users.noreply.github.com>
2026-09-04 23:33:23 +0800

    Revise IROTextBuffer APIs

--- src/TextBuffers/FluidX.TextBuffers/Contracts/IReadOnlyTextBuffer.cs
+++ src/TextBuffers/FluidX.TextBuffers/Contracts/IReadOnlyTextBuffer.cs
@@ -1,16 +1,19 @@
 ﻿namespace FluidX.TextBuffers;
 
+/// <summary>
+/// Represents a read-only view of a text buffer that provides access to the raw text content of a document.
+/// </summary>
 public interface IReadOnlyTextBuffer : IEquatable<IReadOnlyTextBuffer>
 {
     /**
      * Definitions:
      * - Length: string length of a piece of text (Unicode characters outside the BMP have a length of 2).
      * - Line number: number of lines from the start of the document (the first line in the document has line number 1).
-     * - Column number: length of characters from the start of the document (the first character in a line has column number 1),
-     *                  including line break characters ("\r\n" occupy 2 columns),
+     * - Column number: length of characters from the start of the line (the first character in a line has column number 1),
+     *                  including line break characters ("\r\n" has length 2, "\n" and "\r" have length 1),
      *                  high surrogate characters occupy 2 columns.
      * - Offset: length of characters from the first character in the document (the first character in the document has offset 0),
-     *           including line break characters ("\r\n" has length 2).
+     *           including end-of-line sequences in previous lines ("\r\n" has length 2, "\n" and "\r" have length 1).
      * - Character count: number of Unicode characters in a piece of text (Unicode characters outside the BMP have a count of 1).
      */
 
@@ -19,91 +22,42 @@ public interface IReadOnlyTextBuffer : IEquatable<IReadOnlyTextBuffer>
     /// <summary>
     /// The Byte Order Mark of the document (either <see cref="string.Empty"/> or U+FEFF)
     /// </summary>
+    [Obsolete("This will be lifted to TextModel")]
     string BOM { get; }
 
     /// <summary>
     /// The end-of-line sequence of the document (either CRLF, CR or LF)
     /// <para>If the document has mixed end-of-line sequences, the majority one is returned.</para>
     /// </summary>
+    [Obsolete("This will be lifted to TextModel, the text buffer will not have a document level EOL property. The plain EOL sequences are stored for all lines.")]
     string GetEOL();
 
     /// <summary>
     /// <see langword="true"/> if the document might contain RTL characters
     /// </summary>
+    [Obsolete("This will be lifted to TextModel")]
     bool MightContainRTL { get; }
 
     /// <summary>
     /// <see langword="true"/> if the document might contain LINE SEPARATOR (LS) or PARAGRAPH
     /// </summary>
+    [Obsolete("Unusual line terminators will not be supported. They will be treated as general Unicode characters.")]
     bool MightContainUnusualLineTerminators { get; }
 
     /// <summary>
     /// Reset the state of the buffer to indicate that it doesn't contain unusual line terminators
     /// </summary>
+    [Obsolete("Unusual line terminators will not be supported. They will be treated as general Unicode characters.")]
     void ResetMightContainUnusualLineTerminators();
 
     /// <summary>
     /// <see langword="true"/> if the document might contain non-ASCII content
     /// </summary>
+    [Obsolete("This will be lifted to TextModel")]
     bool MightContainNonBasicASCII { get; }
 
-    #endregion
-
-    #region Position and Range
-
     /// <summary>
-    /// Get the length of characters from the first character in the document of a specified character.
-    /// </summary>
-    /// <param name="lineNumber">Line number of the specified character</param>
-    /// <param name="column">Column number of the specified character</param>
-    /// <returns>Length of characters from the first character in the document.</returns>
-    int GetOffsetAt(int lineNumber, int column);
-
-    /// <summary>
-    /// Get the <see cref="TextPosition"/> at an <paramref name="offset"/> from the start of the document.
-    /// </summary>
-    /// <param name="offset">Length of characters from the first character in the document</param>
-    /// <returns><see cref="TextPosition"/> of the character at the given <paramref name="offset"/>.</returns>
-    TextPosition GetPositionAt(int offset);
-
-    /// <summary>
-    /// Get the <see cref="TextRange"/> of text of a given <paramref name="length"/> at a given <paramref name="offset"/>.
-    /// </summary>
-    /// <param name="offset">Length of characters from the first character in the document</param>
-    /// <param name="length">Length of the text in the <see cref="TextRange"/></param>
-    /// <returns>The <see cref="TextRange"/> of the text specified.</returns>
-    TextRange GetRangeAt(int offset, int length);
-
-    #endregion
-
-    #region Text operations
-
-    /// <summary>
-    /// Get the text in a <see cref="TextRange"/> with the specified <see cref="EndOfLinePreference"/>.
-    /// </summary>
-    /// <param name="range"><see cref="TextRange"/> of text</param>
-    /// <param name="eol">End-of-line preference</param>
-    /// <returns>Text in <paramref name="range"/> with the specified <see cref="EndOfLinePreference"/>.<returns>
-    string GetValueInRange(TextRange range, EndOfLinePreference eol);
-
-    /// <summary>
-    /// Get the length of text in a <see cref="TextRange"/> with the specified <see cref="EndOfLinePreference"/>.
-    /// </summary>
-    /// <param name="range">TextRange of text</param>
-    /// <param name="eol">End-of-line preference</param>
-    /// <returns>Length of text in <paramref name="range"/> with the specified <see cref="EndOfLinePreference"/>.</returns>
-    int GetValueLengthInRange(TextRange range, EndOfLinePreference eol);
-
-    /// <summary>
-    /// Get the number of Unicode characters in a <see cref="TextRange"/> with the specified <see cref="EndOfLinePreference"/>.
-    /// </summary>
-    /// <param name="range">TextRange of text</param>
-    /// <param name="eol">End-of-line preference</param>
-    /// <returns>Number of characters in <paramref name="range"/> with the specified <see cref="EndOfLinePreference"/>.</returns>
-    int GetCharacterCountInRange(TextRange range, EndOfLinePreference eol);
-
-    /// <summary>
-    /// Total length of characters in the document.
+    /// Total number of characters in the document, including end-of-line sequences. High surrogate characters occupy a length of 2.
     /// </summary>
     int Length { get; }
 
@@ -112,54 +66,92 @@ public interface IReadOnlyTextBuffer : IEquatable<IReadOnlyTextBuffer>
     /// </summary>
     int LineCount { get; }
 
+    #endregion
+
+    #region Position, Range and Offset Conversions
+
     /// <summary>
-    /// Get all lines in the document.
+    /// Get the length of characters from the first character in the document of a character at the
+    /// specified position, including end-of-line sequences in previous lines ("\r\n" has length 2,
+    /// "\n" and "\r" have length 1).
     /// </summary>
-    /// <returns>A list of lines</returns>
+    /// <param name="position">Position of the specified character.</param>
+    /// <returns>Length of characters from the first character in the document.</returns>
+    int GetOffsetAt(TextPosition position);
+
+    /// <summary>
+    /// Get the <see cref="TextPosition"/> corresponding to an <paramref name="offset"/> in the document.
+    /// </summary>
+    /// <param name="offset">Length of characters from the first character in the document (the first character in the document has offset 0),
+    /// including end-of-line sequences in previous lines("\r\n" has length 2, "\n" and "\r" have length 1).</param>
+    /// <returns><see cref="TextPosition"/> of the character at the given <paramref name="offset"/>.</returns>
+    TextPosition GetPositionAt(int offset);
+
+    /// <summary>
+    /// Get the <see cref="TextRange"/> of corresponding to a text of a given <paramref name="length"/> at a given <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the first character to be included in the range.</param>
+    /// <param name="length">Length of the text to be included from the given <paramref name="offset"/>.</param>
+    /// <returns>The <see cref="TextRange"/> of the text specified.</returns>
+    TextRange GetRangeAt(int offset, int length);
+
+    #endregion
+
+    #region Text operations
+
+    // TODO: Consider adding a general API that iterates over a range of text with zero allocation and compute a property from the text range.
+    // This replaces GetCharacterCountInRange, GetLineFirstNonWhitespaceColumn, GetLineLastNonWhitespaceColumn, etc.
+
+    /// <summary>
+    /// Get the text in a <see cref="TextRange"/>.
+    /// </summary>
+    /// <param name="range">Range of the text.</param>
+    /// <returns>Text in the specified <paramref name="range"/>.</returns>
+    string GetTextInRange(TextRange range);
+
+    /// <summary>
+    /// Get the length of text in a <see cref="TextRange"/>.
+    /// </summary>
+    /// <param name="range">Range of the text.</param>
+    /// <returns>Length of text in <paramref name="range"/>.</returns>
+    int GetTextLengthInRange(TextRange range);
+
+    /// <summary>
+    /// Get the number of Unicode characters in a <see cref="TextRange"/> with the specified <see cref="EndOfLinePreference"/>.
+    /// </summary>
+    /// <param name="range">TextRange of text</param>
+    /// <param name="eol">End-of-line preference</param>
+    /// <returns>Number of characters in <paramref name="range"/> with the specified <see cref="EndOfLinePreference"/>.</returns>
+    [Obsolete("This is a high-level API, Unicode character count can be computed from string content outside text buffer.")]
+    int GetCharacterCountInRange(TextRange range, EndOfLinePreference eol);
+
+    /// <summary>
+    /// Get all lines in the document as a list of <see langword="string"/>.
+    /// The end-of-line sequence is not included in the content of each line.
+    /// </summary>
+    /// <returns>A read-only view of the list of lines.</returns>
     IReadOnlyList<string> GetLinesContent();
 
     /// <summary>
     /// Get the content of a line, excluding the end-of-line sequence.
     /// </summary>
-    /// <param name="lineNumber">Line number of the line (starts from 1)</param>
+    /// <param name="lineNumber">Line number of the line.</param>
     /// <returns>Content of the line at <paramref name="lineNumber"/>.</returns>
     string GetLineContent(int lineNumber);
 
     /// <summary>
-    /// Get the <see langword="char"/> at <paramref name="index"/>.
+    /// Get the end-of-line sequence of a line.
     /// </summary>
-    /// <param name="lineNumber">Line number of the character</param>
-    /// <param name="index">Offset from the first character of the line</param>
-    /// <returns>The <see langword="char"/> at offset <paramref name="index"/> in line <paramref name="lineNumber"/>.</returns>
-    char GetLineCharCode(int lineNumber, int index);
-
-    /// <summary>
-    /// Get the <see langword="char"/> at <paramref name="offset"/>.
-    /// </summary>
-    /// <param name="offset">Offset from the first character of the document</param>
-    /// <returns><see langword="char"/> at <paramref name="offset"/>.</returns>
-    char GetCharCode(int offset);
+    /// <param name="lineNumber">Line number of the line.</param>
+    /// <returns>The end-of-line sequence of the line at <paramref name="lineNumber"/>, either CR, LF or CRLF.</returns>
+    string GetLineEOL(int lineNumber);
 
     /// <summary>
     /// Get the length of characters in a line, excluding the end-of-line sequence.
     /// </summary>
-    /// <param name="lineNumber">Line number of the line</param>
+    /// <param name="lineNumber">Line number of the line.</param>
     /// <returns>Length of characters in line <paramref name="lineNumber"/>.</returns>
     int GetLineLength(int lineNumber);
-
-    /// <summary>
-    /// Get the minimum column number in a line.
-    /// </summary>
-    /// <param name="lineNumber">Line number of the line</param>
-    /// <returns>Minimum column number of line <paramref name="lineNumber"/>.</returns>
-    int GetLineMinColumn(int lineNumber);
-
-    /// <summary>
-    /// Get the maximum column number in a line.
-    /// </summary>
-    /// <param name="lineNumber">Line number of the line</param>
-    /// <returns>Maximum column number in line <paramref name="lineNumber"/>.</returns>
-    int GetLineMaxColumn(int lineNumber);
 
     /// <summary>
     /// Get the first non-whitespace column number in a line.
@@ -175,9 +167,43 @@ public interface IReadOnlyTextBuffer : IEquatable<IReadOnlyTextBuffer>
     /// <returns>Column number of the last non-whitespace character</returns>
     int GetLineLastNonWhitespaceColumn(int lineNumber);
 
+    /// <summary>
+    /// Get the <see langword="char"/> at a given <see cref="TextPosition"/>.
+    /// </summary>
+    /// <param name="position">Position of the character.</param>
+    /// <returns>The <see langword="char"/> at the given <paramref name="position"/>.</returns>
+    char GetCharCode(TextPosition position);
+
+    /// <summary>
+    /// Get the <see langword="char"/> at a given <paramref name="offset"/>.
+    /// </summary>
+    /// <param name="offset">Offset of the character.</param>
+    /// <returns>The <see langword="char"/> at the given <paramref name="offset"/>.</returns>
+    char GetCharCode(int offset);
+
+    /// <summary>
+    /// Get the minimum column number in a line.
+    /// </summary>
+    /// <param name="lineNumber">Line number of the line</param>
+    /// <returns>Minimum column number of line <paramref name="lineNumber"/>.</returns>
+    [Obsolete("The minimum column number is always 1.")]
+    int GetLineMinColumn(int lineNumber);
+
+    /// <summary>
+    /// Get the maximum column number in a line.
+    /// </summary>
+    /// <param name="lineNumber">Line number of the line</param>
+    /// <returns>Maximum column number in line <paramref name="lineNumber"/>.</returns>
+    [Obsolete("The maximum column number is equal to the length of the line plus 1.")]
+    int GetLineMaxColumn(int lineNumber);
+
     #endregion
 
+    // TODO: Review
+
     ITextSnapshot CreateSnapshot(bool preserveBOM);
+
+    // TODO: Review
 
     IReadOnlyList<FindMatch> FindMatchesLineByLine(
         TextRange searchRange,
